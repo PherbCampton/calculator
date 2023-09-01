60401ffe2e268ba81b12df6bef14f3890c937fb9
Samuel Tuinperi <tuinperisamuel@yahoo.com>
2023-09-01 15:41:40 +0100

    updated

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const themeToggle = document.getElementById("themeToggle");
 const toggleText = document.querySelector(".toggleText");
+const equal = document.querySelector("#equal");
+const form = document.querySelector("form");
+const inputs = document.querySelectorAll("input");
+const display = document.getElementById("display");
 const body = document.body;
 
 themeToggle.addEventListener("click", () => {
@@ -12,4 +16,23 @@ themeToggle.addEventListener("click", () => {
     body.classList.add("light-theme");
     toggleText.innerHTML = "SWITCH TO DARK MODE";
   }
+});
+
+form.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    equal.click();
+  }
+});
+
+inputs.forEach((e) =>
+  e.addEventListener("click", function (event) {
+    event.preventDefault();
+    display.focus();
+  })
+);
+
+display.addEventListener("input", () => {
+  // When input changes, set the cursor position to the end
+  display.scrollRight = display.scrollWidth;
 });
